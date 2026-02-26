e339e5a72f45d96547d32c6c0438b827587ebe31
FineFur <177952045+FineFur@users.noreply.github.com>
2026-02-26 20:50:20 +0530

    Update TestClass.java

--- Method Reusability/src/TestClass.java
+++ Method Reusability/src/TestClass.java
@@ -1,3 +1,5 @@
+public class TestClass 
+{
 	public static void main(String[] args) 
 	{
 		Calculator cal = new Calculator();
@@ -5,10 +7,10 @@
 		double res_1 = cal.addTwo(10, 20);
 		System.out.println("Addition is : " + res_1);
 		
-		double res_2 = cal.addTwo(10, 20);
+		double res_2 = cal.average(10, 20);
 		System.out.println("Average = " + res_2);
 		
-		double res_3 = cal.addTwo(10, 20);
+		double res_3 = cal.discount(10, 20);
         System.out.println("Discount = " + res_3);
 	}
 }
